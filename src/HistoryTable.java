41a757ef2cf03c5168a0196b5d56900508282c1b
PThinh440 <npthinh212@clc.fitus.edu.vn>
2023-11-08 10:23:35 +0700

    Add basic HistoryTable class

---
+++ src/HistoryTable.java
@@ -0,0 +1,18 @@
+import java.util.List;
+
+public class HistoryTable {
+    public static List<String> wordHistory;
+
+    public static void addToHistory(String word){
+
+    }
+
+    public static boolean loadHistory(){
+        // should make this in constructor
+        return true;
+    }
+
+    public static void main(String[] args){
+
+    }
+}
