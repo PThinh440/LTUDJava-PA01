ccd86bc0eb888d115a864a236a29a3e981ebbbb9
PThinh440 <npthinh212@clc.fitus.edu.vn>
2023-11-08 10:25:48 +0700

    Add EventListener with handling events

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.util.Objects;
 
 class EventListener implements ActionListener{
     public void actionPerformed(ActionEvent e)
@@ -13,6 +15,33 @@ class EventListener implements ActionListener{
                 Main.frame.dispose();
                 Main.frame = UI_Search.createFrame();
                 break;
+            case "Find": {
+                String text = UI_Search.textInput.getText().toUpperCase();
+
+                if (text.isEmpty()) {
+                    JOptionPane.showMessageDialog(null, "Input is empty!");
+                    break;
+                }
+
+                String result = "";
+
+                if (UI_Search.comboBox.getSelectedItem().equals("Find by word")) {
+                    result = DictionaryManager.findWordDefinition(text);
+                } else {
+                    result = DictionaryManager.findDefinitionWord(text);
+                }
+
+                if (result.isEmpty()){
+                    JOptionPane.showMessageDialog(null, "No result");
+                    break;
+                }
+//                System.out.println(result);
+                String[] results = result.split("`");
+
+                UI_Search.searchResult.setListData(results);
+
+                break;
+            }
             case "History":
                 Main.frame.dispose();
                 Main.frame = UI_History.createFrame();
@@ -23,14 +52,54 @@ class EventListener implements ActionListener{
                 Main.frame.dispose();
                 Main.frame = UI_Quiz.createFrame();
                 break;
-//            case "Add":
-//                break;
-//            case "Edit":
-//                break;
-//            case "Delete":
-//                break;
-//            case "Reset":
-//                break;
+            case "Add": {
+                String text = UI_Search.textInput.getText().toUpperCase();
+
+                if (text.isEmpty()) {
+                    JOptionPane.showMessageDialog(null, "Input is empty!");
+                    break;
+                }
+
+                if (DictionaryManager.addNewWord(text)){
+                    JOptionPane.showMessageDialog(null, "Add successfully!");
+                }
+
+                break;
+            }
+            case "Edit": {
+                String text = UI_Search.textInput.getText().toUpperCase();
+
+                if (text.isEmpty()) {
+                    JOptionPane.showMessageDialog(null, "Input is empty!");
+                    break;
+                }
+                if (DictionaryManager.editExistingWord(text)){
+                    JOptionPane.showMessageDialog(null, "Edit successfully!");
+
+                }
+                break;
+            }
+            case "Delete": {
+                String text = UI_Search.textInput.getText().toUpperCase();
+
+                if (text.isEmpty()) {
+                    JOptionPane.showMessageDialog(null, "Input is empty!");
+                    break;
+                }
+                if (DictionaryManager.deleteExistingWord(text)){
+                    JOptionPane.showMessageDialog(null, "Delete successfully!");
+                }
+
+                break;
+            }
+            case "Reset":
+                try {
+                    DictionaryManager.loadDictionary();
+                } catch (IOException ex) {
+                    throw new RuntimeException(ex);
+                }
+                JOptionPane.showMessageDialog(null,"Reset successfully!");
+                break;
             case "Back":
                 Main.frame.dispose();
                 Main.frame = UI_Default.createFrame();
@@ -41,16 +110,16 @@ class EventListener implements ActionListener{
     }
 }
 
-public class Main {
-    public Main() {
-
-    }
-
+public class Main { // 3, 5, 8, 9, 10
     public static JFrame frame;
-
     public static void main(String[] args){
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
+                try {
+                    DictionaryManager.loadDictionary();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
                 frame = UI_Default.createFrame();
             }
         });
