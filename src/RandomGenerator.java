2e192b1d1f20b0e7d1d3695f6fe064ffc53faea2
PThinh440 <npthinh212@clc.fitus.edu.vn>
2023-11-10 10:09:14 +0700

    Add RandomGenerator

---
+++ src/RandomGenerator.java
@@ -0,0 +1,13 @@
+import java.util.Random;
+
+public class RandomGenerator {
+    private static final Random rand = new Random();
+
+    public static Random getRand(){
+        return rand;
+    }
+
+    public static void main(String[] args){
+
+    }
+}
