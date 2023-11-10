a8f4709c6bdbf3de407fc8e9f861d76409f34b99
PThinh440 <npthinh212@clc.fitus.edu.vn>
2023-11-10 09:33:07 +0700

    Improve default UI

--- src/UI_Default.java
+++ src/UI_Default.java
@@ -1,34 +1,61 @@
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 
 public class UI_Default {
     private static void createContentPane(Container container) {
         EventListener listener = new EventListener();
 
+        ///// MAIN PANEL
+        JPanel mainPanel = new JPanel();
+        mainPanel.setBorder(BorderFactory.createTitledBorder("Slang word dictionary"));
+        mainPanel.setLayout(new BorderLayout());
+        mainPanel.setPreferredSize(new Dimension(200,100));
+
         //// SEARCH BUTTON
         JButton button = new JButton("Search");
         button.setActionCommand("Search");
         button.addActionListener(listener);
-        container.add(button, BorderLayout.CENTER);
+
+        /// PADDING for search button
+        JPanel paddingPanel = new JPanel();
+        paddingPanel.setBorder(new EmptyBorder(0, 10, 5, 5));
+        paddingPanel.add(button);
+        mainPanel.add(paddingPanel, BorderLayout.CENTER);
 
         //// HISTORY BUTTON
         button = new JButton("History");
         button.setActionCommand("History");
         button.addActionListener(listener);
-        container.add(button, BorderLayout.LINE_END);
+
+        /// PADDING for history button
+        paddingPanel = new JPanel();
+        paddingPanel.setBorder(new EmptyBorder(0, 5, 5, 10));
+        paddingPanel.add(button);
+        mainPanel.add(paddingPanel, BorderLayout.LINE_END);
 
         //// QUIZ BUTTON
         button = new JButton("Quiz");
         button.setActionCommand("Quiz");
         button.addActionListener(listener);
 
-        container.add(button, BorderLayout.PAGE_END);
+        /// PADDING for quiz button
+        paddingPanel = new JPanel();
+        paddingPanel.add(button);
+        mainPanel.add(button, BorderLayout.PAGE_END);
+
+        //// PADDING for main panel
+        paddingPanel = new JPanel();
+        paddingPanel.setBorder(new EmptyBorder(10, 10, 10, 10));
+        paddingPanel.add(mainPanel);
+
+        container.add(paddingPanel);
     }
 
     public static JFrame createFrame() {
         JFrame.setDefaultLookAndFeelDecorated(true);
         JFrame frame = new JFrame("Dictionary");
-        frame.setPreferredSize(new Dimension(200, 100));
+        frame.setPreferredSize(new Dimension(250, 150));
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         createContentPane(frame.getContentPane());
         frame.pack();
